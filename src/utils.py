72ffd57f83738baf466c02282a4eef42bcbb0c5e
jdgilhuly <jdgilhuly@icloud.com>
2024-10-12 20:03:39 -1000

    updated utils for spark configs

--- src/utils.py
+++ src/utils.py
@@ -1,7 +1,13 @@
 from pyspark.sql import SparkSession
 
-# Initialize Spark Session
-spark = SparkSession.builder \
-    .appName("LoanApprovalETL") \
-    .config("spark.jars.packages", "org.apache.hadoop:hadoop-aws:3.2.0") \
-    .getOrCreate()
+import os
+os.environ['PYSPARK_SUBMIT_ARGS'] = '--packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.1.2 pyspark-shell'
+
+spark = (SparkSession.builder
+         .appName("CreditCardETL")
+         .config("spark.driver.extraJavaOptions", "-Xss4M")
+         .config("spark.executor.extraJavaOptions", "-Xss4M")
+         .config("spark.driver.memory", "4g")
+         .config("spark.executor.memory", "4g")
+         .config("spark.sql.session.timeZone", "UTC")
+         .getOrCreate())
